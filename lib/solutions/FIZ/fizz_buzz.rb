d48b787b765b7d3b629ec97d75e9d13705215214
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:50:46 +0000

    Mon Apr 08 12:50:46 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,14 +3,21 @@ class FizzBuzz
 
   def fizz_buzz(number)
     @number = number
+p '1: '
+p @number
     return @return = 'fizz buzz' if @number % 15 == 0
+p '3: '
+p output_fizz?(number)
     return @return = 'fizz'if output_fizz?(number)
     return @return = 'buzz' if @number % 5 == 0
     return @return = @number.to_s
   end
 
   def output_fizz?(number)
+p '2: '
+p @number
     @number.to_s.include? '3' || @number % 3 == 0
   end
 
 end
+
