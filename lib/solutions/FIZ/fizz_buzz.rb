0939a586961bf7f1a2fc5ceae8716027a1a102c2
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:47:46 +0000

    Mon Apr 08 12:47:46 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,19 +4,13 @@ class FizzBuzz
   def fizz_buzz(number)
     @number = number
     return @return = 'fizz buzz' if @number % 15 == 0
-    return @return = 'fizz'if @number % 3 == 0
+    return @return = 'fizz'if output_fizz?(number)
     return @return = 'buzz' if @number % 5 == 0
     return @return = @number.to_s
   end
 
   def output_fizz?(number)
-    number.to_s.include? '3'
+    @number.to_s.include? '3' || @number % 3 == 0
   end
 
 end
-
-
-
-
-
-
--- test/FIZ/fizz_buzz_test.rb
+++ test/FIZ/fizz_buzz_test.rb
@@ -49,7 +49,3 @@ class ClientTest < Minitest::Test
   # end
 
 end
-
-
-
-
