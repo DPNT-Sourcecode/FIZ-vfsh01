80665df615f78e8a2b207569fcd04a9e22a12810
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:05:51 +0000

    Mon Apr 08 11:05:51 UTC 2019

--- lib/solutions/SUM/sum.rb
+++ lib/solutions/SUM/sum.rb
@@ -2,12 +2,14 @@
 class Sum
 
   def sum(x, y)
-    x + y
+    check(x, y)
+    @return = x + y
   end
 
   def check(x, y)
-    raise 'Need 0-100 integer'
+    raise 'Need 0-100 integer' unless x >= 0 && x <= 100 && y >= 0 && y <= 100
   end
 
 end
+
 
