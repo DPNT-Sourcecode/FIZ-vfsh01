cabac6256836c92c8658f1bc375262cf29d6ee49
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:10:26 +0000

    Mon Apr 08 12:10:26 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,14 +2,17 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    if number == 15
+    if number % 15 == 0
       return 'fizz buzz'
-    elsif number == 3
+    elsif number % 3 == 0
       return 'fizz'
-    elsif number == 5
+    elsif number % 5 == 0
       return 'buzz'
+    else
+      return number.to_s
     end
   end
 end
+
 
 
--- test/FIZ/fizz_buzz_test.rb
+++ test/FIZ/fizz_buzz_test.rb
@@ -17,23 +17,26 @@ class ClientTest < Minitest::Test
   end
 
   def test_buzz
-    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'Num divisible by 3 should output fizz'
+    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'Num divisible by 5 should output buzz'
   end
 
   def test_other_integer
-    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'Num divisible by 3 should output fizz'
+    assert_equal '4', FizzBuzz.new.fizz_buzz(4), 'Num not divisible by 3 or 5 should output num'
   end
-  #
-  # def test_sum_2
-  #   assert_equal 4, Sum.new.sum(1, 3), 'App should add two numbers'
-  # end
-  #
-  # def test_positive_integer
-  #   assert_raises 'Need 0-100 integer' do
-  #     Sum.new.sum(-1, 3)
-  #   end
-  # end
+
+  def test_fizz_buzz
+    assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(30), 'Num divisible by 15 should output fizz buzz'
+  end
+
+  def test_fizz
+    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(6), 'Num divisible by 3 should output fizz'
+  end
+
+  def test_buzz
+    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(10), 'Num divisible by 5 should output buzz'
+  end
 
 end
+
 
 
