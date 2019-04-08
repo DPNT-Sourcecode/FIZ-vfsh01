30c5ec7daafda59d6638400b097f9f69a57f8e9a
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:19:46 +0000

    Mon Apr 08 11:19:46 UTC 2019

---
+++ test/HLO/hlo_test.rb
@@ -0,0 +1,25 @@
+# noinspection RubyResolve,RubyResolve
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'HLO'
+
+class ClientTest < Minitest::Test
+
+  def test_hlo
+    assert_equal "Hello, world!", Hello.new.hello("world"), 'App should return sentence'
+  end
+  #
+  # def test_sum_2
+  #   assert_equal 4, Sum.new.sum(1, 3), 'App should add two numbers'
+  # end
+  #
+  # def test_positive_integer
+  #   assert_raises 'Need 0-100 integer' do
+  #     Sum.new.sum(-1, 3)
+  #   end
+  # end
+
+end
